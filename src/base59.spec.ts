c12477ee524d804114a55f0fe3b0367a88807734
Bogdan Vaneev <warchantua@gmail.com>
2019-10-09 00:39:30 +0300

    Rebase; fix test
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- src/base59.spec.ts
+++ src/base59.spec.ts
@@ -1,17 +1,19 @@
-var fs = require('fs');
-
-import * as enc from './base59';
+import fs from 'fs';
+import { Base59 } from './base59';
 
 describe('base59 test', () => {
   it('check Java - JS similarity', () => {
-    let base59hex  = fs.readFileSync("./../testData/base59.hex.txt", 'utf-8').split(/\r?\n/);
-    let base59java = fs.readFileSync("./../testData/base59.java.txt", 'utf-8').split(/\r?\n/);
+    const base59hex = fs
+      .readFileSync('testData/base59.hex.txt', 'utf-8')
+      .split(/\r?\n/);
+    const base59java = fs
+      .readFileSync('testData/base59.java.txt', 'utf-8')
+      .split(/\r?\n/);
 
     expect(base59hex.length).toEqual(base59java.length);
-    for(let i = 0; i< base59hex.length; i++){
-      let line = base59hex[i];
-      let buff = Buffer.from(line, 'hex');
-      let base59js = enc.Base59.encode(buff);
+    for (let i = 0; i < base59hex.length; i++) {
+      const buff = Buffer.from(base59hex[i], 'hex');
+      const base59js = Base59.encode(buff);
       expect(base59js).toEqual(base59java[i]);
     }
   });
