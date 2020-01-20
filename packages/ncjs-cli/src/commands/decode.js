c089e856dac5a2af004959b8a3c034ddb37c486b
Bohdan Vanieiev <warchantua@gmail.com>
2020-01-20 20:23:35 +0200

    Create a CLI

---
+++ packages/ncjs-cli/src/commands/decode.js
@@ -0,0 +1,152 @@
+const {
+  ReadStream,
+  ATV,
+  VTB,
+  BtcBlock,
+  VbkBlock,
+  BtcTx,
+  Address,
+  Coin,
+  Output,
+  PublicationData,
+  VbkMerklePath,
+  MerklePath,
+  VbkPopTx,
+  VbkTx,
+  Sha256Hash,
+} = require('@veriblock/nodecore-parser');
+
+const { Command, flags } = require('@oclif/command');
+
+const entitiesRead = [
+  'vtb',
+  'atv',
+  'address',
+  'btctx',
+  'coin',
+  'output',
+  'publicationdata',
+  'vbkmerklepath',
+  'vbkblock',
+  'vbktx',
+  'vbkpoptx',
+];
+
+const entitiesReadExtract = ['merklepath', 'btcblock'];
+
+const entitiesAll = [...entitiesRead, ...entitiesReadExtract];
+
+const streamWrap = f => {
+  return str => {
+    const buf = Buffer.from(str, 'hex');
+    const stream = new ReadStream(buf);
+    return f(stream);
+  };
+};
+
+const entitiesReadParsers = {
+  vtb: streamWrap(VTB.read),
+  atv: streamWrap(ATV.read),
+  address: streamWrap(Address.read),
+  btctx: streamWrap(BtcTx.read),
+  coin: streamWrap(Coin.read),
+  output: streamWrap(Output.read),
+  publicationdata: streamWrap(PublicationData.read),
+  vbkmerklepath: streamWrap(VbkMerklePath.read),
+  vbkblock: streamWrap(VbkBlock.read),
+  vbktx: streamWrap(VbkTx.read),
+  vbkpoptx: streamWrap(VbkPopTx.read),
+};
+
+const entityFlag = name =>
+  flags.build({
+    description: `hexencoded ${name.toUpperCase()}`,
+    exclusive: entitiesAll.filter(f => f !== name),
+  });
+
+class DecodeCommand extends Command {
+  async run() {
+    const { flags } = this.parse(DecodeCommand);
+
+    if (Object.keys(flags).length === 0) {
+      await DecodeCommand.run(['-h']);
+      return this.exit(0);
+    }
+
+    if ('merklepath' in flags) {
+      // @ts-ignore
+      const subject = Sha256Hash.fromHex(flags.subject);
+      // @ts-ignore
+      const raw = flags.raw;
+      // @ts-ignore
+      const path = flags.merklepath;
+
+      let json;
+      if (raw) {
+        json = streamWrap(s => MerklePath.extract(s, subject))(path);
+      } else {
+        json = streamWrap(s => MerklePath.read(s, subject))(path);
+      }
+
+      this.log(JSON.stringify(json, null, 2));
+      return this.exit(0);
+    }
+
+    if ('btcblock' in flags) {
+      const raw = flags.raw;
+      const block = flags.btcblock;
+
+      let json;
+      if (raw) {
+        json = streamWrap(BtcBlock.extract)(block);
+      } else {
+        json = streamWrap(BtcBlock.read)(block);
+      }
+
+      this.log(JSON.stringify(json, null, 2));
+      return this.exit(0);
+    }
+
+    const key = Object.keys(flags)[0];
+    // @ts-ignore
+    if (entitiesReadParsers[key] === undefined) {
+      this.error('Unknown entity type');
+      return this.exit(1);
+    }
+
+    // @ts-ignore
+    const json = entitiesReadParsers[key](flags[key]);
+    this.log(JSON.stringify(json, null, 2));
+    return this.exit(0);
+  }
+}
+
+DecodeCommand.description = `Decode command decodes entities stored as VBK-encoded byte arrays.`;
+
+DecodeCommand.examples = `$ ncjs-cli decode --address 01166772F51AB208D32771AB1506970EEB664462730B838E
+$ ncjs-cli decode --raw --btcblock 000040203f8e3980304439d853c302f6e496285e110e251251531300000000000000000039a72c22268381bd8d9dcfe002f472634a24cf0454de8b50f89e10891e5ffb1de08d9b5c6c1f2c1744290a92
+$ ncjs-cli decode --btcblock 50000040203f8e3980304439d853c302f6e496285e110e251251531300000000000000000039a72c22268381bd8d9dcfe002f472634a24cf0454de8b50f89e10891e5ffb1de08d9b5c6c1f2c1744290a92
+$ ncjs-cli decode --subject 94E097B110BA3ADBB7B6C4C599D31D675DE7BE6E722407410C08EF352BE585F1 --merklepath <...>
+`;
+
+DecodeCommand.flags = entitiesRead.reduce(
+  (acc, cur) => {
+    // @ts-ignore
+    acc[cur] = entityFlag(cur)();
+    return acc;
+  },
+  {
+    help: flags.help({ char: 'h' }),
+    raw: flags.boolean({
+      description: 'Use raw (non-VBK) encoding when possible',
+    }),
+    subject: flags.string({
+      description:
+        'Manually specified MerklePath subject (32 bytes hexencoded hash)',
+    }),
+    merklepath: entityFlag('merklepath')({ dependsOn: ['subject'] }),
+    btcblock: entityFlag('btcblock')(),
+  }
+);
+
+module.exports = DecodeCommand;
