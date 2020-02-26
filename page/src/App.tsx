04935a01355eab027dd024b8bebba9b20e3b24a6
Bohdan Vanieiev <warchantua@gmail.com>
2020-02-26 14:34:47 +0200

    Fix

--- page/src/App.tsx
+++ page/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import {
@@ -11,12 +11,27 @@ import {
   Output,
   PublicationData,
   ReadStream,
+  Sha256Hash,
   VbkBlock,
   VbkMerklePath,
   VbkPopTx,
   VbkTx,
   VTB,
 } from '@veriblock/nodecore-parser';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Container,
+  Row,
+  Col,
+  FormCheck,
+  Dropdown,
+  InputGroup,
+  DropdownButton,
+  FormControl,
+  Form,
+  Button,
+  ButtonGroup,
+} from 'react-bootstrap';
 require('codemirror/lib/codemirror.css');
 require('codemirror/theme/material.css');
 require('codemirror/theme/neat.css');
@@ -51,13 +66,28 @@ const extractOrRead = (raw: boolean, data: string, extract: any, read: any) => {
   }
 };
 
-const decode = (data: string, raw: boolean, entity: string) => {
+const decode = (
+  data: string,
+  raw: boolean,
+  entity: string,
+  subject: string
+) => {
   if (entity === 'vbkblock') {
     return extractOrRead(raw, data, VbkBlock.extract, VbkBlock.read);
   }
 
   if (entity === 'btcblock') {
     return extractOrRead(raw, data, BtcBlock.extract, BtcBlock.read);
+  }
+
+  if (entity === 'merklepath') {
+    const sub = Sha256Hash.fromHex(subject);
+    return extractOrRead(
+      raw,
+      data,
+      (s: ReadStream) => MerklePath.extract(s, sub),
+      (s: ReadStream) => MerklePath.read(s, sub)
+    );
   }
 
   if (entity in entitiesReadParsers) {
@@ -68,76 +98,118 @@ const decode = (data: string, raw: boolean, entity: string) => {
   throw new Error('undefined entity type');
 };
 
-const options = {
+const codeOptions = {
   mode: 'string',
   theme: 'material',
   lineWrapping: true,
   lineNumbers: true,
 };
 
+const jsonOptions = {
+  mode: {
+    name: 'javascript',
+    json: true,
+  },
+  theme: 'material',
+  lineNumbers: true,
+  lineWrapping: true,
+  readOnly: 'nocursor',
+};
+
 function App() {
-  const [subject, setSubject] = useState('');
-  const [raw, setRaw] = useState(false);
+  const [subject, setSubject] = useState(
+    '0000000000000000000000000000000000000000000000000000000000000000'
+  );
+  const [raw, setRaw] = useState(true);
   const [entity, setEntity] = useState('btcblock');
+  const [decoded, setDecoded] = useState('');
+  const [needSubject, setNeedSubject] = useState(false);
   const [hex, setHex] = useState(
     '000040203f8e3980304439d853c302f6e496285e110e251251531300000000000000000039a72c22268381bd8d9dcfe002f472634a24cf0454de8b50f89e10891e5ffb1de08d9b5c6c1f2c1744290a92'
   );
-  const [decoded, setDecoded] = useState('');
-  const [needSubject, setNeedSubject] = useState(false);
+  useEffect(() => {
+    setNeedSubject(entity === 'vbkmerklepath' || entity === 'merklepath');
 
-  setNeedSubject(entity === 'merklepath' || entity === 'vbkmerklepath');
+    try {
+      const d = decode(hex, raw, entity, subject);
+      setDecoded(JSON.stringify(d, null, 2));
+    } catch (e) {
+      setDecoded(`${e.stack}`);
+    }
+  }, [hex, raw, entity]);
 
   return (
-    <div className="App">
-      <div className="menu">
-        <select id="entity" onChange={e => setEntity(e.target.value)}>
-          <option value="btcblock">BtcBlock</option>
-          <option value="vbkblock">VbkBlock</option>
-        </select>
-        <input
-          type="checkbox"
-          id="raw"
-          name="raw"
-          value="Raw"
-          onChange={e => setRaw(e.target.checked)}
-        />
-        <label htmlFor="raw">Raw</label>
-        {needSubject && (
-          <div className="subject">
-            <input type="text" id="subject" name="subject" value={subject} />
-            <label htmlFor="subject">Subject</label>
-          </div>
-        )}
-      </div>
-      <div className="left">
-        <CodeMirror
-          value={hex}
-          options={options}
-          onBeforeChange={(editor, data, value) => {
-            setHex(value);
-            try {
-              const d = decode(value, raw, entity);
-              setDecoded(d);
-            } catch (e) {
-              setDecoded(e);
-            }
-          }}
-          onChange={(editor, value) => {}}
-        />
-      </div>
-      {/*<div className="right">*/}
-      {/*  <CodeMirror*/}
-      {/*    value={decoded}*/}
-      {/*    options={{ ...options, readOnly: true }}*/}
-      {/*    onBeforeChange={(editor, data, value) => {*/}
-      {/*      console.log('onBeforeChange');*/}
-      {/*    }}*/}
-      {/*    onChange={(editor, value) => {*/}
-      {/*      console.log('controlled', { value });*/}
-      {/*    }}*/}
-      {/*  />*/}
-      {/*</div>*/}
-    </div>
+    <Container className="App">
+      <Row className="menu">
+        <Col xs={6}>
+          <Form.Group controlId="formEntity">
+            <Form.Label>Entity</Form.Label>
+            <select
+              value={entity}
+              className="form-control"
+              onChange={e => setEntity(e.target.value)}
+            >
+              <option value="atv">ATV</option>
+              <option value="vtb">VTB</option>
+              <option value="publicationdata">PublicationData</option>
+              <option value="btcblock">BtcBlock</option>
+              <option value="vbkblock">VbkBlock</option>
+              <option value="address">Address</option>
+              <option value="coin">Coin</option>
+              <option value="merklepath">MerklePath</option>
+              <option value="output">Output</option>
+              <option value="vbkpoptx">VbkPopTx</option>
+              <option value="vbktx">VbkTx</option>
+              <option value="btctx">BtcTx</option>
+            </select>
+          </Form.Group>
+        </Col>
+        <Col xs={6}>
+          <Form.Group controlId="formSubject">
+            <Form.Label>Subject</Form.Label>
+            <Form.Control
+              type="text"
+              value={subject}
+              disabled={!needSubject}
+              onChange={(e: any) => setSubject(e.target.value)}
+              placeholder="Sha256 hexstring..."
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form.Check
+            checked={raw}
+            onChange={(e: any) => setRaw(e.target.checked)}
+            type="checkbox"
+            label="Raw"
+          />
+        </Col>
+      </Row>
+      <Row className="left">
+        <Col>
+          <CodeMirror
+            value={hex}
+            options={codeOptions}
+            onBeforeChange={(editor, data, value) => {
+              setHex(value);
+            }}
+            onChange={(editor, value) => {}}
+          />
+        </Col>
+      </Row>
+      <Row className="right">
+        <Col>
+          <CodeMirror
+            value={decoded}
+            options={jsonOptions}
+            onBeforeChange={(editor, data, value) => {}}
+            onChange={(editor, value) => {}}
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
