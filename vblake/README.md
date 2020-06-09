171b51643801029e2e6357424296bb06aaa3114f
Bohdan Vanieiev <warchantua@gmail.com>
2020-06-09 13:30:51 +0300

    Publish vblake 1.0.1

--- vblake/README.md
+++ vblake/README.md
@@ -14,28 +14,5 @@ https://unpkg.com/@veriblock/vblake@1.0.0/vblake_module.js
 
 Result is `vblake_module.js`. Include it in your HTML, and use:
 ```
-const hash = Module.vblake('123');
-```
-
-You must pass `bytes` as javascript string! 
-Returned argument is also bytes.
-
-Use these to get bytes-from-hex and hex-from-bytes:
-```javascript
-// Convert a hex string to a byte array
-function hexToBytes(hex) {
-    for (var bytes = [], c = 0; c < hex.length; c += 2)
-    bytes.push(parseInt(hex.substr(c, 2), 16));
-    return bytes;
-}
-
-// Convert a byte array to a hex string
-function bytesToHex(bytes) {
-    for (var hex = [], i = 0; i < bytes.length; i++) {
-        var current = bytes[i] < 0 ? bytes[i] + 256 : bytes[i];
-        hex.push((current >>> 4).toString(16));
-        hex.push((current & 0xF).toString(16));
-    }
-    return hex.join("");
-}
+const hash = Module.vblake('0123');
 ```
--- vblake/bindings.cpp
+++ vblake/bindings.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <iomanip>
+#include <sstream>
 #include <emscripten/bind.h>
 
 extern "C" {
@@ -7,10 +9,36 @@ extern "C" {
 
 using namespace emscripten;
 
+std::vector<char> HexToBytes(const std::string& hex) {
+  std::vector<char> bytes;
+
+  for (unsigned int i = 0; i < hex.length(); i += 2) {
+    std::string byteString = hex.substr(i, 2);
+    char byte = (char) strtol(byteString.c_str(), NULL, 16);
+    bytes.push_back(byte);
+  }
+
+  return bytes;
+}
+
+std::string hexStr(std::string in)
+{
+     std::stringstream ss;
+     ss << std::hex;
+
+     for( int i = 0; i < in.size(); ++i )
+         ss << std::setw(2) << std::setfill('0') << (int)in[i];
+
+     return ss.str();
+}
+
 std::string vblake_glue(std::string in) {
     std::string out(VBLAKE_HASH_SIZE, 0);
-    vblake((void*)out.data(), (const void*)in.data(), in.size());
-    return out;
+    auto n = HexToBytes(in);
+    if(0 != vblake((void*)out.data(), (const void*)n.data(), n.size())){
+        return "";
+    }
+    return hexStr(out);
 }
 
 EMSCRIPTEN_BINDINGS(vblake) {
